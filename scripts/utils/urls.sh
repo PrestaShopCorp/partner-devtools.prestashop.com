8e11cafcc9b5d95015e6a365e40e34dea278e68a
TakeshiDaveau <takeshi.daveau@prestashop.com>
2022-05-04 15:04:31 +0200

    fix(tunnel): replace RBM_NAME by ADMIN_MAIL

--- scripts/utils/urls.sh
+++ scripts/utils/urls.sh
@@ -5,7 +5,7 @@ source ./scripts/utils/read_env.sh
 ENV_FILE=.env
 TUNNEL_DOMAIN=$(read_var TUNNEL_DOMAIN $ENV_FILE)
 RBM_NAME=$(read_var RBM_NAME $ENV_FILE)
-RBM_NAME=$(read_var ADMIN_MAIL $ENV_FILE)
+ADMIN_MAIL=$(read_var ADMIN_MAIL $ENV_FILE)
 SUBDOMAIN_NAME=`docker logs ps-tunnel.local 2>/dev/null | awk -F '/' '{print $3}' | awk -F"." '{print $1}' | awk 'END{print}' | tr -d "[:space:]"`
 
 get_fo_url() {
