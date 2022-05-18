e0b4f8f281999da81f3136140475efc100c18549
Emmanuel Gautier <git@emmanuelgautier.fr>
2022-05-18 11:14:23 +0200

    chore: add account_example

---
+++ modules/account_example/_dev/apps/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <account-example />
+  </div>
+</template>
+
+<script>
+import AccountExample from "@/components/AccountExample.vue";
+
+export default {
+  name: 'App',
+  components: {
+    AccountExample
+  },
+  setup() {
+    console.log('account example initialized');
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modules/account_example/_dev/apps/src/components/AccountExample.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="pt-2">
+    <section>
+      <div class="p-0 m-auto tw-container">
+        <PsAccounts></PsAccounts>
+      </div>
+    </section>
+
+    <div v-if="sub && sub.id">
+      Display your configuration, only if customer is associated
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccountExample',
+  components: {
+    PsAccounts: async () => {
+      let psAccounts = window?.psaccountsVue?.PsAccounts;
+      if (!psAccounts) {
+        console.log('Fallback to Account Vue component');
+        psAccounts = require('prestashop_accounts_vue_components').PsAccounts;
+      }
+
+      return psAccounts;
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+section {
+  margin-bottom: 35px;
+}
+</style>
