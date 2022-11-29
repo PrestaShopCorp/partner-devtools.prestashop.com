d0e99d9b978a93270643ef6f1893ae1870fe6cbb
Fabien Moreau <fmoreau.go@gmail.com>
2022-11-29 15:45:10 +0100

    Vuepress/theming (#106)
    Co-authored-by: Fabien Moreau <fMoreau@prestamac.home>
    Co-authored-by: Fabien Moreau <fMoreau@prestamac-216.home>
    Co-authored-by: Fabien Moreau <fMoreau@PRESTAMAC-216.local>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'SaaS App by Prestashop',
   description: '',
-  theme: 'api',
+  theme: '',
   head: [
     ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/images/favicons/apple-touch-icon.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/images/favicons/favicon-32x32.png"}],
@@ -13,4 +13,28 @@ module.exports = {
     ['meta', { name: "msapplication-config", content: "/assets/images/favicons/browserconfig.xml"}],
     ['meta', { name: "theme-color", content: "#011638"}],
   ],
+  themeConfig: {
+    displayAllHeaders: true,
+    sidebarDepth: 2,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Overview', link: '/0-overview/' },
+      { text: 'Relationships', link: '/1-relationships/' },
+      { text: 'SaaS app', link: '/2-saas-app/' },
+      { text: 'Webhook Events', link: '/3-webhook-events/' },
+      { text: 'Setup dev environment', link: '/4-setup-dev-environment/' },
+      { text: 'API', link: '/5-api/' },
+      { text: 'FAQ', link: '/6-faq/' }
+    ],
+    sidebar: [
+      [ '/' ,'Home' ],
+      [ '/0-overview/', 'Overview' ],
+      [ '/1-relationships/', 'Relationships' ],
+      [ '/2-saas-app/', 'SaaS app' ],
+      [ '/3-webhook-events/', 'Webhook Events' ],
+      [ '/4-setup-dev-environment/', 'Setup dev environment'],
+      [ '/5-api/', 'API' ],
+      [ '/6-faq/', 'FAQ' ]
+    ]
+  }
 }
