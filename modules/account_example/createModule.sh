d2cee4ec226752a1e70d788147ae1696fd85cae5
atourneriePresta <anthony.tournerie@prestashop.com>
2022-12-06 12:43:59 +0100

    feat: create a zip in bash

---
+++ modules/account_example/createModule.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ..
+zip -r account_example.zip account_example -x '*.git*' \
+          account_example/_dev/\* \
+          account_example/dist/\* \
+          account_example/composer.phar \
+          account_example/Makefile \
+          account_example/account_example.zip
+mv account_example.zip ./account_example
